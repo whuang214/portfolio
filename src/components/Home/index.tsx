c8edf2121787a29c51cb24cd29bce45bfcadd88b
whuang214 <whuang2146@gmail.com>
2023-09-08 01:12:47 -0400

    add components files

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home: React.FC = () => (
+  <section id="home" className="h-screen flex items-center justify-center bg-blue-400 text-white">
+    <div>
+      <h1 className="text-5xl mb-4">Welcome to My Portfolio</h1>
+      <p className="text-lg">Introductory text or a tagline here.</p>
+    </div>
+  </section>
+);
+
+export default Home;
